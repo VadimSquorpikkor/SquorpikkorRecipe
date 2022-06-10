d9b4a5d7be6037c08c4a646020b25f8d4d2a5358
VadimSquorpikkor <uniqueehub1024>
2022-06-10 15:30:45 +0300

    10.06.22 1. Полностью переписаны стили 2. Добавлен prism.css и prism.js, теперь pre элемент смотрится круто

--- style_func.js
+++ style_func.js
@@ -1,12 +1,13 @@
 
 
 //Get the button:
-mybutton = document.getElementById("myBtn");
+// let mybutton = document.getElementById("myBtn");
 
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
+    let mybutton = document.getElementById("myBtn");
     if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
         mybutton.style.display = "block";
     } else {
